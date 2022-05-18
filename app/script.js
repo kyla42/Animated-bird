fb6e4dc6a23467c1e74361cb2ec3c8a047a13690
kyla42 <103237579+kyla42@users.noreply.github.com>
2022-05-18 23:58:18 +0900

    updated

--- app/script.js
+++ app/script.js
@@ -1,5 +1,5 @@
 const birdBtn = document.querySelector(".bird");
-const movement = ["shake", "jump", "stretch1", "stretch2", "ribbon", "sliding", "incline"];
+const movement = ["shake", "jump", "stretch1", "stretch2", "ribbon", "sliding", "incline", "glasses"];
 
 birdBtn.addEventListener("click", function () {
     for (let i = 0; i < movement.length; i++) {
